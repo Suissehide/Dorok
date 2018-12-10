eeac5b5d5faa2323846b3f219928a6274a01482f
tlaulan <to.laulan@hotmail.fr>
2018-12-10 16:41:31 +0100

    Add begin of SOrt function

---
+++ app/src/main/java/fr/playtipus/dorok/MapCreation/Sort.java
@@ -0,0 +1,21 @@
+package fr.playtipus.dorok.MapCreation;
+
+import java.util.List;
+
+import fr.playtipus.dorok.Tile;
+
+public class Sort {
+    private List<Tile> lSorted;
+
+    public  Sort (List<Tile> lTiles) {
+        /*X == 0 && Y le plus petit;
+        while ()
+        X -= 1;
+        Y += 1;
+        puis X = 1 && Y le plus petit;
+        find puis met a la place i ++ ? */
+    }
+    public List<Tile> getlTiles() {
+        return lSorted;
+    }
+}
